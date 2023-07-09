41050a55162f2fc52ccd3cf97214188fff3a9e99
Mohammad Zafar <mzcoder7@gmail.com>
2023-07-09 18:47:24 +0100

    date and source issue fixed

--- news-app/src/components/HomePage/ResultsArea/ResultsArea.js
+++ news-app/src/components/HomePage/ResultsArea/ResultsArea.js
@@ -16,13 +16,23 @@ export default function ResultsArea(props) {
 						image={article.image}
 						description={article.description}
 						url={article.url}
-						source={article.source.name}
-						date={new Date(article.publishedAt).toLocaleDateString('en-GB', {
-							weekday: 'long',
-							year: 'numeric',
-							month: 'long',
-							day: 'numeric',
-						})}
+						// read source.name on homepage for each news item, on saved articles page, it is just a 'source' key
+						source={
+							window.location.pathname === '/'
+								? article.source.name
+								: article.source
+						}
+						// on the homepage, format date the first time it is read from each news item object. One saved articles page, it is already added as a 'date' key for each item, so no need to format it again.
+						date={
+							window.location.pathname === '/'
+								? new Date(article.publishedAt).toLocaleDateString('en-GB', {
+										weekday: 'long',
+										year: 'numeric',
+										month: 'long',
+										day: 'numeric',
+								  })
+								: article.date
+						}
 					/>
 				</div>
 			))}
